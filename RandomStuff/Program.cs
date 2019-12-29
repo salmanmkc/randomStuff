05e58fc162ced70f4c6002d0290d38df3c5d173c
SalmanMKC <13schishti@gmail.com>
2019-12-29 16:50:19 +0000

    added blazor project

---
+++ RandomStuff/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RandomStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            this isn't blazor??????????????????????????????
+            Console.WriteLine("Hello World!");
+
+            so how do we do blazor
+            will set up 
+            one second
+            
+
+            okay
+            
+        }
+    }
+}
